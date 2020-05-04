6ca9b02bc68a0ab1c8903d47401a2a84b3da793b
MCH <magnushalle8@live.dk>
2020-05-04 21:01:51 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to optionality
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject optionality "0.1.0-SNAPSHOT"
+  :description "API for fetching option data"
+  :url "https://github.com/IYIagnus/optionality"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns optionality.core})
